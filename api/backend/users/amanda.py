4407739a7c7de489ac39627c79cc7faa21eb8320
Saumya Palakodety <89223433+saumyapalk23@users.noreply.github.com>
2025-08-11 10:50:32 -0400

    amanda changes

--- api/backend/users/amanda.py
+++ api/backend/users/amanda.py
@@ -21,11 +21,7 @@ amanda = Blueprint('amanda', __name__)
 def top_reviewed():
 
     cursor = db.get_db().cursor()
-    cursor.execute('''SELECT s.showId, s.title, COUNT(r.reviewId) AS num_reviews
-FROM shows s JOIN reviews r ON s.showId = r.showId
-GROUP BY s.showId, s.title
-ORDER BY num_reviews DESC
-LIM 5;
+    cursor.execute('''
     ''')
     
     theData = cursor.fetchall()
